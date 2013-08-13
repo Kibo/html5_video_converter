10eb15af258823e17245273b94475b288547052a
Tomas Jurman <tomasjurman@gmail.com>
2013-08-13 14:24:17 +0200

    v0.1

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-html5_video_converter
-=====================
-
+# HTML5 video converter
 Bash script for converting video to HTML5 formats with FFmpeg.
+
+###Feature
+- Convert video file to format to WebM, OGG, MP4.
+- Automatic conversion of folder with video files
+
+###Using
+- ./web_video_converter.sh MY_VIDEO_FILE.avi
+- ./web_video_converter.sh FOLDER_WITH_VIDEOS
+
+###More information
+- [Converting video for web with FFmpeg](http://tomasjurman.blogspot.cz/2013/08/converting-video-for-web-with-ffmpeg.html)
+
+###Contact me
+- tomasjurman@gmail.com
+
---
+++ web_video_converter.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# It converts input video file or files in directory to WebM/Vorbis, OGG/Vorbis, H.264/ACC formats.
+# Example: ./web_video_converter.sh VIDEO_FILE_NAME
+# Example: ./web_video_converter.sh DIR_TO_VIDEOS
+
+OUTPUT_DIR="VIDEOS/"
+BIT_RATE="345k"
+
+# There is not parameter or -h
+if [ "$#" = "0" ] || [ "$1" = "-h" ]; then
+	echo -e "\n\tIt converts input video file or files in directory to WebM/Vorbis, OGG/Vorbis, H.264/ACC formats.\n\tExample: ./web_video_converter.sh myVideo.avi\n\tExample: ./web_video_converter.sh dirWithVideos\n"
+	exit 0
+fi
+
+function convertFile {
+	# Filename without extension
+	filename=$(basename "$1")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+
+	# WebM/ Vorbis
+	ffmpeg -i $1 -acodec libvorbis -vcodec libvpx -b $BIT_RATE ${OUTPUT_DIR}${filename}.webm
+
+	# OGG/ Vorbis
+	ffmpeg -i $1 -acodec libvorbis -vcodec libtheora -b $BIT_RATE ${OUTPUT_DIR}${filename}.ogg
+
+	# H.264/ACC
+	# -acodec libfaac
+	# @see http://trac.ffmpeg.org/wiki/CompilationGuide
+	ffmpeg -i $1 -acodec libvo_aacenc -vcodec libx264 -b $BIT_RATE ${OUTPUT_DIR}${filename}.mp4
+}
+
+# Is directory
+if [[ -d $1 ]]; then
+	# List files in directory	
+	for file in $(find $1 -type f); 
+	do
+		convertFile $file	
+	done  
+# Is file  
+elif [[ -f $1 ]]; then    
+	convertFile $1
+else
+    echo -e "$1 does not file or directory."
+    exit 1
+fi
+
+exit 0
